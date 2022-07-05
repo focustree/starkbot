735d95dc3218652bbba5727a184065bc6c816c7d
thomas auguste <thomas.auguste92@gmail.com>
2022-07-06 01:05:40 +0200

    problem with constructor

--- apps/starknet/script-test.mjs
+++ apps/starknet/script-test.mjs
@@ -8,7 +8,17 @@ import {
   number,
 } from "starknet";
 
+/// Defining provider
+console.log("Defining provider");
+
+const provider = new Provider({
+  baseUrl: 'http://127.0.0.1:5050',
+  feederGatewayUrl: 'http://127.0.0.1:5050',
+  gatewayUrl: 'http://127.0.0.1:5050',
+});
+
 /// Creating new account
+console.log("Creating new account");
 
 const starkKeyPair = ec.genKeyPair();
 const starkKeyPub = ec.getStarkKey(starkKeyPair);
@@ -16,22 +26,15 @@ const starkKeyPub = ec.getStarkKey(starkKeyPair);
 const compiledAccount = json.parse(
   fs.readFileSync("./contract/account.json").toString("ascii")
 );
-const accountResponse = await defaultProvider.deployContract({
+const accountResponse = await provider.deployContract({
   contract: compiledAccount,
   addressSalt: starkKeyPub,
 });
 
 const account = new Account(provider, accountResponse.address, starkKeyPair);
 
-/// Defining provider
-
-const provider = new Provider({
-    baseUrl: 'http://127.0.0.1:5050',
-    feederGatewayUrl: 'http://127.0.0.1:5050',
-    gatewayUrl: 'http://127.0.0.1:5050',
-});
-
 /// Deploying Starknet ID smart contract on-chain
+console.log("Deploying Starknet ID smart contract on-chain")
 
 const compiled_starknet_id = json.parse(
   fs.readFileSync("./contract/starknet_id.json").toString("ascii")
@@ -41,13 +44,14 @@ const starknet_id_response = await provider.deployContract({
   contract: compiled_starknet_id,
 });
 
-console.log("Waiting for Tx to be Accepted on Starknet DevNet - Straknet ID Deployment...");
+console.log("Waiting for Tx to be Accepted on Starknet DevNet - Starknet ID Deployment...");
 await provider.waitForTransaction(starknet_id_response.transaction_hash);
 
 const starknet_id_contract_address = starknet_id_response.address;
 const starknet_id_contract = new Contract(compiled_starknet_id.abi, starknet_id_contract_address);
 
 /// Deploying NFT smart contract on-chain
+console.log("Deploying NFT smart contract on-chain")
 
 const compiled_nft = json.parse(
   fs.readFileSync("./contract/nft.json").toString("ascii")
@@ -55,13 +59,18 @@ const compiled_nft = json.parse(
 
 const nft_response = await provider.deployContract({
   contract: compiled_nft,
+  inputs: [1234, 1234, account.address],
 });
 
 console.log("Waiting for Tx to be Accepted on Starknet DevNet - NFT Deployment...");
 await provider.waitForTransaction(nft_response.transaction_hash);
 
+console.log("Defining contract");
+
 const nft_contract_address = nft_response.address;
 const nft_contract = new Contract(compiled_nft.abi, nft_contract_address);
+
+console.log("NFT contract successfully deployed");
 
 /// Minting NFT for the user we created
 
