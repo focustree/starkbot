c4406ac558a6253f6994a19cb3cc09958e1c1f82
Gabin Marignier <gabin.contact@gmail.com>
2022-07-06 00:13:01 +0200

    Improve modal

--- apps/discord-bot/src/bot/commands/addRule.ts
+++ apps/discord-bot/src/bot/commands/addRule.ts
@@ -12,33 +12,19 @@ export const AddRule: Command = {
   name: 'starkbot-add-rule',
   description: 'Assign a role based on owned NFTs',
   run: async (client: Client, interaction: BaseCommandInteraction) => {
-    const modal = new Modal().setCustomId('myModal').setTitle('My Modal');
-    // Add components to modal
-    // Create the text input components
-    const favoriteColorInput = new TextInputComponent()
-      .setCustomId('favoriteColorInput')
-      // The label is the prompt the user sees for this input
-      .setLabel("What's your favorite color?")
-      // Short means only a single line of text
+    const modal = new Modal().setCustomId('addRule').setTitle('Add Rule');
+
+    const tokenAdressInput = new TextInputComponent()
+      .setCustomId('tokenAddress')
+      .setLabel('Token contract address')
       .setStyle('SHORT');
-    const hobbiesInput = new TextInputComponent()
-      .setCustomId('hobbiesInput')
-      .setLabel("What's some of your favorite hobbies?")
-      // Paragraph means multiple lines of text.
-      .setStyle('PARAGRAPH');
-    // An action row only holds one text input,
-    // so you need one action row per text input.
-    const firstActionRow =
+
+    modal.addComponents(
       new MessageActionRow<ModalActionRowComponent>().addComponents(
-        favoriteColorInput
-      );
-    const secondActionRow =
-      new MessageActionRow<ModalActionRowComponent>().addComponents(
-        hobbiesInput
-      );
-    // Add inputs to the modal
-    modal.addComponents(firstActionRow, secondActionRow);
-    // Show the modal to the user
+        tokenAdressInput
+      )
+    );
+
     await interaction.showModal(modal);
   },
 };
