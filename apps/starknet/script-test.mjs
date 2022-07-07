d160f3684b89bc0fd31f37eb2c6f014c27fd677f
thomas auguste <thomas.auguste92@gmail.com>
2022-07-07 19:08:52 +0200

    problem with script

--- apps/starknet/script-test.mjs
+++ apps/starknet/script-test.mjs
@@ -6,6 +6,9 @@ import {
   Provider,
   json,
   number,
+  stark,
+  shortString,
+  uint256
 } from "starknet";
 
 /// Defining provider
@@ -57,9 +60,17 @@ const compiled_nft = json.parse(
   fs.readFileSync("./contract/nft.json").toString("ascii")
 );
 
+const account_address_int = parseInt(account.address, 16);
+
+const constructor_data = stark.compileCalldata({
+  name: shortString.encodeShortString("NFT identity"),
+  symbol: shortString.encodeShortString("nft"),
+  owner: account.address
+});
+
 const nft_response = await provider.deployContract({
   contract: compiled_nft,
-  inputs: [1234, 1234, account.address],
+  constructorCalldata : constructor_data
 });
 
 console.log("Waiting for Tx to be Accepted on Starknet DevNet - NFT Deployment...");
@@ -68,24 +79,31 @@ await provider.waitForTransaction(nft_response.transaction_hash);
 console.log("Defining contract");
 
 const nft_contract_address = nft_response.address;
-const nft_contract = new Contract(compiled_nft.abi, nft_contract_address);
+const nft_contract = new Contract(compiled_nft.abi, nft_contract_address, account);
+
+console.log(nft_contract_address);
+console.log(account.address);
 
 console.log("NFT contract successfully deployed");
 
 /// Minting NFT for the user we created
+console.log("Minting NFT for the user we created");
 
 const { transaction_hash: mint_nft_TxHash } = await nft_contract.mint(
   account.address,
-  1234
+  uint256.bnToUint256(1234)
 );
 console.log(`Waiting for Tx to be Accepted on Starknet - Minting NFT...`);
 await provider.waitForTransaction(mint_nft_TxHash);
 
 /// Minting ID for the user we already created
+console.log("Minting ID for the user we already created");
 
-const { transaction_hash: mint_starknet_id_TxHash } = await starknet_id_contract.mint(
-  account.address,
-  1234
-);
+/*const minting_id_data = stark.compileCalldata({
+  tokenId: uint256.bnToUint256(1234)
+});
+
+const { transaction_hash: mint_starknet_id_TxHash } = await starknet_id_contract.invoke("mint", [minting_id_data]);
 console.log(`Waiting for Tx to be Accepted on Starknet - Minting Starknet ID...`);
 await provider.waitForTransaction(mint_starknet_id_TxHash);
+*/
